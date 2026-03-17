290b640f14226a5493dc77e3ee5155470e34deba
dashutka06-lab <dashutka06@gmail.com>
2026-03-17 13:19:15 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+
+const CACHE_NAME='family-skylight-v1';
+const urls=['./','./index.html','./kristina.html','./samuil.html','./pollyanna.html','./mama.html','./styles.css','./app.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urls))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
